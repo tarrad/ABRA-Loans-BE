3456a84e3f929bcbcb528473eeaf269be51b3c7f
tarrad <44922599+tarrad@users.noreply.github.com>
2025-03-11 17:49:25 +0200

    removed non relevant comments

--- ABRA Loans/Program.cs
+++ ABRA Loans/Program.cs
@@ -6,6 +6,7 @@ using ABRA_Loans.Services;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+
 builder.Services.AddSingleton<IConfiguration>(configuration);
 builder.Services.AddSingleton<LoanUnder20Engine>();
 builder.Services.AddSingleton<LoanBetween20And35Engine>();
@@ -14,7 +15,6 @@ builder.Services.AddSingleton<IClientRepository, ClientRepositoryMock>();
 builder.Services.AddSingleton<LoanHandler>();
 builder.Services.AddSingleton<LoanCalculationService>();
 
-// Add services to the container.
 
 builder.Services.AddCors(options =>
 {
@@ -28,14 +28,12 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 app.UseCors("AllowLocalhost");
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
